9e578e755ba979637411211659fe9a108b85c949
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-02-24 23:54:23 -0500

    Update 01_hello_world.py

--- 01_hello_world.py
+++ 01_hello_world.py
@@ -1,1 +1,9 @@
 print("Hello World!")
+
+counter = 101          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "Jean"       # A string
+
+print counter
+print miles
+print name
